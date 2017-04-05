75c253f33f8052ec93e10e75cebe60c841e6e240
mfc <1034406291@qq.com>
2017-04-05 16:32:10 +0800

    comment->.

--- main.py
+++ main.py
@@ -13,87 +13,90 @@ def main(argv):
         for user in users:
             get_cookie(user)
         exit(0)
-    f = open('namelist', 'r', encoding='utf8')
-    f.readline()
-    name = f.readline().strip()
-    print('开始对' + name + '进行操作')
-    if name == 'movie':
-        cmd = input('1:get_movie_id  2:get_movie_info  3:get_movie_comments\n')
-        if cmd == '1':
-            mysql_tool = MysqlCurd('douban_movie')
-            mysql_tool.connect_mysql()
-            for line in f:
-                if not line.strip() == ''\
-                        and not mysql_tool.query_mysql_condition('movie_name', [{'name': line.strip()},
-                                                                                ['name']]):
-                    mysql_tool.insert_mysql('movie_name', {'name': line.strip()})
-            # 将名单存好
-            for _ in range(0, 1):
-                get_movie_id()
-                print('过滤第' + str(_) + '次!')
-            mysql_tool.delete_mysql('movie_name', {'version': 0})
-            mysql_tool.close_connect()
+    elif argv[1] == 'shell':
+        f = open('namelist', 'r', encoding='utf8')
+        f.readline()
+        name = f.readline().strip()
+        print('开始对' + name + '进行操作')
+        if name == 'movie':
+            cmd = input('1:get_movie_id  2:get_movie_info  3:get_movie_comments\n')
+            if cmd == '1':
+                mysql_tool = MysqlCurd('douban_movie')
+                mysql_tool.connect_mysql()
+                for line in f:
+                    if not line.strip() == '' \
+                            and not mysql_tool.query_mysql_condition('movie_name', [{'name': line.strip()},
+                                                                                    ['name']]):
+                        mysql_tool.insert_mysql('movie_name', {'name': line.strip()})
+                # 将名单存好
+                for _ in range(0, 1):
+                    get_movie_id()
+                    print('过滤第' + str(_) + '次!')
+                mysql_tool.delete_mysql('movie_name', {'version': 0})
+                mysql_tool.close_connect()
 
-        elif cmd == '2':
-            print('首先获取所有电影的id')
-            mysql_tool = MysqlCurd('douban_movie')
-            mysql_tool.connect_mysql()
-            for line in f:
-                if not line.strip() == ''\
-                        and not mysql_tool.query_mysql_condition('movie_name', [{'name': line.strip()},
-                                                                                ['name']]):
-                    mysql_tool.insert_mysql('movie_name', {'name': line.strip()})
-            # 将名单存好
-            for _ in range(0, 1):
-                get_movie_id()
-                print('过滤第' + str(_) + '次!')
-            mysql_tool.delete_mysql('movie_name', {'version': 0})
-            print('获得所有电影信息')
-            for _ in range(0, 1):
-                get_movie_info()
-                print('过滤第' + str(_) + '次!')
-            mysql_tool.delete_mysql('name_id', {'version': 0})
+            elif cmd == '2':
+                print('首先获取所有电影的id')
+                mysql_tool = MysqlCurd('douban_movie')
+                mysql_tool.connect_mysql()
+                for line in f:
+                    if not line.strip() == '' \
+                            and not mysql_tool.query_mysql_condition('movie_name', [{'name': line.strip()},
+                                                                                    ['name']]):
+                        mysql_tool.insert_mysql('movie_name', {'name': line.strip()})
+                # 将名单存好
+                for _ in range(0, 1):
+                    get_movie_id()
+                    print('过滤第' + str(_) + '次!')
+                mysql_tool.delete_mysql('movie_name', {'version': 0})
+                print('获得所有电影信息')
+                for _ in range(0, 1):
+                    get_movie_info()
+                    print('过滤第' + str(_) + '次!')
+                mysql_tool.delete_mysql('name_id', {'version': 0})
 
-        elif cmd == '3':
-            for line in f:
-                if not line.strip() == '':
-                    get_movie_info(line.strip())
-                    get_movie_comment(line.strip())
+            elif cmd == '3':
+                for line in f:
+                    if not line.strip() == '':
+                        get_movie_info(line.strip())
+                        get_movie_comment(line.strip())
 
-    elif name == 'person':
-        cmd = input('1:get_person_id  2:get_person_info\n')
-        if cmd == '1':
-            mysql_tool = MysqlCurd('douban_person')
-            mysql_tool.connect_mysql()
-            for line in f:
-                if not line.strip() == '':
-                    mysql_tool.insert_mysql('person_name', {'name': line.strip()})
-            # 将名单存好
-            for _ in range(0, 2):
-                get_person_id()
-                print('过滤第' + str(_) + '次!')
-            mysql_tool.delete_mysql('person_name', {'version': 0})
-            mysql_tool.close_connect()
-        elif cmd == '2':
-            print('首先获取所有人物的id')
-            mysql_tool = MysqlCurd('douban_person')
-            mysql_tool.connect_mysql()
-            for line in f:
-                if not line.strip() == '':
-                    mysql_tool.insert_mysql('person_name', {'name': line.strip()})
-            # 将名单存好
-            for _ in range(0, 2):
-                get_person_id()
-                print('过滤第' + str(_) + '次!')
-            mysql_tool.delete_mysql('person_name', {'version': 0})
+        elif name == 'person':
+            cmd = input('1:get_person_id  2:get_person_info\n')
+            if cmd == '1':
+                mysql_tool = MysqlCurd('douban_person')
+                mysql_tool.connect_mysql()
+                for line in f:
+                    if not line.strip() == '':
+                        mysql_tool.insert_mysql('person_name', {'name': line.strip()})
+                # 将名单存好
+                for _ in range(0, 2):
+                    get_person_id()
+                    print('过滤第' + str(_) + '次!')
+                mysql_tool.delete_mysql('person_name', {'version': 0})
+                mysql_tool.close_connect()
+            elif cmd == '2':
+                print('首先获取所有人物的id')
+                mysql_tool = MysqlCurd('douban_person')
+                mysql_tool.connect_mysql()
+                for line in f:
+                    if not line.strip() == '':
+                        mysql_tool.insert_mysql('person_name', {'name': line.strip()})
+                # 将名单存好
+                for _ in range(0, 2):
+                    get_person_id()
+                    print('过滤第' + str(_) + '次!')
+                mysql_tool.delete_mysql('person_name', {'version': 0})
 
-            for _ in range(0, 2):
-                get_person_info()
-                print('过滤第' + str(_) + '次!')
-            mysql_tool.delete_mysql('person_name_id', {'version': 0})
+                for _ in range(0, 2):
+                    get_person_info()
+                    print('过滤第' + str(_) + '次!')
+                mysql_tool.delete_mysql('person_name_id', {'version': 0})
+        else:
+            print('请在namelist中更改类型')
+            exit(0)
     else:
-        print('请在namelist中更改类型')
-        exit(0)
+        print('please input your command')
     exit(0)
 
 
