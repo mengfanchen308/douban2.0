1fa5105b7ebc3af20305d5e8cc7d8d4ae4e9a45a
mfc <1034406291@qq.com>
2017-04-05 19:24:02 +0800

    bug xiufu

--- comment/movie.py
+++ comment/movie.py
@@ -160,14 +160,15 @@ def get_movie_comment(movie_name):
     tool = MysqlCurd('douban_movie')
     tool.connect_mysql()
     try:
-        data = tool.query_mysql_condition('movie_info', [{'movie_name': movie_name}, ['movie_id', 'movie_name',
-                                                                                  'comment_num']])[0]
+        data = tool.query_mysql_condition('movie_info', [{'movie_name': movie_name}, ['movie_id',
+                                                                                      'movie_name',
+                                                                                      'comment_num']])[0]
     except IndexError:
         print('dont have this movie id!')
         return False
     print(data[1] + ' started!' + str(data[2]) + 'comments!')
     m_table = Movie(data[0], data[1])
-    m_table.tool.create_table(m_table.name, {'user_name': 'varchar(300) not null primary key',
+    m_table.tool.create_table(m_table.name, {'user_name': 'varchar(200) not null primary key',
                                              'score': 'int',
                                              'comment_time': 'varchar(45)',
                                              'vote': 'int',
